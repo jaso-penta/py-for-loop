61ae6b06a255093f6f7d40c1b2186bbb459f785f
Jašo <andrija.pericic@gmail.com>
2025-09-09 20:03:27 +0200

    Create fot_loop_04.py

---
+++ fot_loop_04.py
@@ -0,0 +1,27 @@
+'''
+Napisite program koji ispisuje trokut tako da su linije stranica prikazane zvjezdicama *.
+
+      *
+     **
+    * *
+   *  *
+  *   *
+ *    *
+*******
+'''
+
+
+# pitajmo korisnika koji znak želi koristiti
+hight = int(input("Unesite visinu trokuta: " ))
+symbol = str(input('Unesite znak koji želite koristiti za ispis trokuta: ')  )
+
+
+print(f'{(hight - 1) * ' '}{symbol}')
+
+for number in range(hight - 2):
+    space_before = hight - 2 - number
+    print(f"{space_before * ' '}{symbol}{' ' * number}{symbol}")
+
+
+print(f'{symbol * hight}') 
+
